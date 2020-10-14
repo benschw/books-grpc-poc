88e11b5437f727ec55778bd1fcbef676b5eb57d2
Ben Schwartz <benschw@gmail.com>
2020-10-14 10:22:25 -0500

    comments updates

--- internal/fakes/repo.go
+++ internal/fakes/repo.go
@@ -9,19 +9,19 @@ import (
 // Ensure Repo implements internal.BooksRepo.
 var _ internal.Repo = &Repo{}
 
-// Repo manages fake database access for books_old
+// Repo manages fake database access for books
 type Repo struct {
 	i     uint64
 	Books []*books.Book
 }
 
-// NewRepo creates a new postgres repo
+// NewRepo creates a new in memory repo (create an implementation backed by a db for persistence)
 func NewRepo() *Repo {
 
 	return &Repo{i: 0, Books: []*books.Book{}}
 }
 
-// FindAll returns all books_old from the database
+// FindAll returns all books from the database, optionally filtered by author
 func (r *Repo) FindAll(query *books.BookQuery) ([]*books.Book, error) {
 	if query.Author == "" {
 		return r.Books, nil
@@ -35,7 +35,7 @@ func (r *Repo) FindAll(query *books.BookQuery) ([]*books.Book, error) {
 	return matches, nil
 }
 
-// Create adds a new book to the databases
+// Create adds a new book to the database
 func (r *Repo) Create(book *books.Book) (*books.Book, error) {
 	r.i = r.i + 1
 	book.Id = r.i
