5656f788b1bd6c73e801af7009171c7f8a65f6c3
Ben Schwartz <benschw@gmail.com>
2020-10-20 13:27:44 -0500

    backing out changes for bootstrap branch

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,76 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"github.com/benschw/books-grpc-poc/pkg/pb/books"
-	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-	"io"
-	"log"
-)
-
-
-var (
-	addr = flag.String("addr", "localhost:9000", "The server address in the format of host:port")
-	cmd = flag.String("cmd", "", "The client command to run (add or list)")
-	author = flag.String("author", "", "author value for adding a book or searching by author")
-	title = flag.String("title", "", "title value for adding a book")
-)
-
+import "fmt"
 
 func main() {
-
-	flag.Parse()
-
-	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect: %s", err)
-	}
-	defer conn.Close()
-
-	c := books.NewBookServiceClient(conn)
-
-	switch *cmd {
-	case "add":
-		if err := Add(c, *author, *title); err != nil {
-			log.Fatalf("add - error adding book: %s", err)
-		}
-		break;
-	case "list":
-		if err := List(c, *author); err != nil {
-			log.Fatalf("list - error listing books: %s", err)
-		}
-		break;
-	default:
-		log.Fatalf("unknown command: %s", *cmd)
-	}
-}
-
-func Add(c books.BookServiceClient, author string, title string) error {
-	book, err := c.AddBook(context.Background(), &books.Book{Author: author, Title: title})
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Book Added: %v\n", book)
-	return nil
-}
-
-func List(c books.BookServiceClient, author string) error {
-	bookStream, err := c.FindAllBooks(context.Background(), &books.BookQuery{Author: author})
-	if err != nil {
-		return err
-	}
-	for {
-		book, err := bookStream.Recv()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return err
-		}
-		fmt.Printf("%v\n", book)
-	}
-	return nil
+	fmt.Println("Hello World")
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,36 +1,7 @@
 package main
 
-import (
-	"flag"
-	"github.com/benschw/books-grpc-poc/internal/app"
-	"github.com/benschw/books-grpc-poc/internal/fakes"
-	"github.com/benschw/books-grpc-poc/pkg/pb/books"
-	"google.golang.org/grpc"
-	"log"
-	"net"
-)
-
-var (
-	addr = flag.String("addr", "localhost:9000", "The server address in the format of host:port")
-)
+import "fmt"
 
 func main() {
-	flag.Parse()
-
-	repo := fakes.NewRepo()
-
-	lis, err := net.Listen("tcp", *addr)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := app.NewServer(repo)
-
-	grpcServer := grpc.NewServer()
-
-	books.RegisterBookServiceServer(grpcServer, s)
-
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %s", err)
-	}
-
+	fmt.Println("Hello World")
 }
